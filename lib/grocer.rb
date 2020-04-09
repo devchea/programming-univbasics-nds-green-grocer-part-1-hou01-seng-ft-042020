176c5272a1d54b394faaf04a5bf561b135b4ef3b
Richard Chea <rchea817@gmail.com>
2020-04-09 20:57:48 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -22,7 +22,7 @@ def consolidate_cart(cart)
   checkout_cart = []
   cart.each do |cart_item|
     item_name = cart_item[:item]
-    item_info = find_item_by_name_in_collection (cart_item[:item], checkout_cart)
+    item_info = find_item_by_name_in_collection (item_name, checkout_cart)
     if
 
     end
