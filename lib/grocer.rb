b65de3a01354e51b5176f9f10212af0565aa8603
Richard Chea <rchea817@gmail.com>
2020-04-09 21:00:10 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -25,5 +25,14 @@ def consolidate_cart(cart)
     item_info = find_item_by_name_in_collection (item_name, checkout_cart)
     if item_info
       item_info[:count] += 1
-
+    else
+      checkout_cart << {
+        :item => item_name,
+        :price => cart_item[:price],
+        :clearance => cart_item[:clearance],
+        :count => 1
+      }
     end
+  end
+  checkout_cart
+end
